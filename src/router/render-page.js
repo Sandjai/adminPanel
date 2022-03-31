6f7c832975e046c04491f981b2481e1d74d8d3c8
Grigory <grinsdot1@gmail.com>
2022-03-31 12:25:52 +0300

    adminPanel aded

--- src/router/render-page.js
+++ src/router/render-page.js
@@ -2,8 +2,14 @@ export default async function(path, match) {
   const main = document.querySelector('main');
  
   main.classList.add('is-loading');
+  let _path;
+  if (path === 'adminPanel') {
+    _path = 'dashboard';
+  } else {
+    _path = path;
+  }
 
-  const { default: Page } = await import(/* webpackChunkName: "[request]" */`../pages/${path}/index.js`);
+  const { default: Page } = await import(/* webpackChunkName: "[request]" */`../pages/${_path}/index.js`);
   
   const page = new Page(match[1]);
   const element = await page.render();
