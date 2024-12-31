8c687db17ca29e9a474b0f3623a07a4b8c1c0d85
Grigory Semakin <grinsdot1@gmail.com>
2024-12-31 14:41:12 +0300

    Removes match from renderPage

--- src/router/render-page.js
+++ src/router/render-page.js
@@ -1,11 +1,13 @@
-export default async function(path, match) {
+export default async function (path) {
   const main = document.querySelector('main');
- 
-  main.classList.add('is-loading'); 
 
-  const { default: Page } = await import(/* webpackChunkName: "[request]" */`../pages/${path}/index.js`);
-  
-  const page = new Page(match[1]);
+  main.classList.add('is-loading');
+
+  const { default: Page } = await import(
+    /* webpackChunkName: "[request]" */ `../pages/${path}/index.js`
+  );
+
+  const page = new Page();
   const element = await page.render();
 
   main.classList.remove('is-loading');
